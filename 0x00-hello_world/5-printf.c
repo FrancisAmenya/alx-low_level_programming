b3c83ee86aae3e3f7c7484b4d7cf230979fd68b8
FrancisAmenya <francisamenya@live.com>
2023-03-16 19:46:43 -0700

    changed no. 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 
 /**
  * main - prints the phrase with proper grammar,
- * but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero otherwise
+ * but the outcome is a piece of art, the adds a new line
+ * Return: 0 if exited properly, non-zero is a failure
  */
 int main(void)
 {
