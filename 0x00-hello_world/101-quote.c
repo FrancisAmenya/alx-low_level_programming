362d5924c81fce0f1bb4c94258747ca82effaa51
FrancisAmenya <francisamenya@live.com>
2023-03-20 19:04:50 -0700

    added array, then print it to keep lines  < 80

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,14 @@
 #include <unistd.h>
 
 /**
- * main - prints an assigned text then goes to a new li to the standardd error o
+ * main - prints an assigned text then goes to a new li to the standardd error
  * Return: 0 is a good output, others have failed
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char s[];
+
+	s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, sent, 59);
 	return (1);
 }
