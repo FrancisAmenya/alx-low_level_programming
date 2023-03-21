0b9981dff64f1ef3d36812ffcbc3ff7b1aa8be0a
FrancisAmenya <francisamenya@live.com>
2023-03-21 07:56:34 -0700

    a function and a header to print text

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description - _putchar prototype
+ *
+ * Return: 0
+ **/
+
+int main(void)
+{
+	char c;
+
+	c = "_putchar";
+	putchar(c);
+
+	return (0);
+}
