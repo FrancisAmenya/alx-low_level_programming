251d513ad977510cf9385849fc9ed611a2884da8
FrancisAmenya <francisamenya@live.com>
2023-03-23 08:39:24 -0700

    a date printer and formatter

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,4 +7,8 @@ void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
 
+int convert_day(int a, int b);
+
+void print_remaining_days(int a, int b, int c);
+
 #endif /* MAIN_H */
