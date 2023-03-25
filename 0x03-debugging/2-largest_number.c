c8d9f1cfc0978212894e857c9392ba245eb2a951
FrancisAmenya <francisamenya@live.com>
2023-03-24 18:46:07 -0700

    a integer size comparison

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -18,7 +18,7 @@ int largest_number(int a, int b, int c)
 		largest = a;
 	else if (b > a && a > c)
 		largest = b;
-	else if (b > c && a > c)
+	else if (b > c && c > a)
 		largest = b;
 	else
 		largest = c;
