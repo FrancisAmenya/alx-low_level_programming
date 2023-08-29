02265c0f548a8c943265bd94d16682a7e6bfcfa7
FrancisAmenya <amenyafrancis@gmail.com>
2023-08-29 07:49:41 -0700

    a custom main.h header file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _puts_recursion(char *pr);
+
+int _putchar(char c);
+
+void _print_rev_recursion(char *prr);
+
+int _strlen_recursion(char *sr);
+
+int factorial(int f);
+
+int _pow_recursion(int p, int r);
+
+int _sqrt_recursion(int sr);
+
+int is_prime_number(int ipm);
+
+int is_palindrome(char *ip);
+
+int wildcmp(char *s1, char *s2);
+
+#endif
