d4dd5b8a0d163f64a71c4b51f65343c5b6ece3a4
FrancisAmenya <amenyafrancis@gmail.com>
2023-08-29 19:00:57 -0700

    sezen

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,10 +12,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 unsigned int inti = 0;
 listint_t *tempers = head;
 
-while (tempers && (inti + 1 < index +1))
+while (tempers && (inti + 1 < index + 1))
 {
 tempers = tempers->next;
-inti+=1;
+inti += 1;
 }
 
 return (tempers ? tempers : NULL);
