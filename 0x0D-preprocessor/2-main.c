5ab78ea22df95858055ad213ffe32e6c640438d0
FrancisAmenya <amenyafrancis@gmail.com>
2023-08-30 12:36:39 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - specifier prints the name of the file
+ *
+ * Return: specifier Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
