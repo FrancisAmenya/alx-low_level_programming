3f0e386ee0e09606d4873e600b78f9d1b0766a49
FrancisAmenya <amenyafrancis@gmail.com>
2024-07-31 16:09:54 -0700

    0x00 fix 12b

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - print numbers 00 to 99
 * description - nested loop
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-	int a;
+	int i, j;
 
-	for (a = 0; a < 100; a++)
+	for (i = 0; i <= 98; i++)
 	{
-		putchar('0' + a / 10);
-		putchar('0' + a % 10);
-		if (!(a == 99))
+		for (j = i + 1; j <= 99; j++)
 		{
-			putchar(',');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
+}
 	putchar('\n');
 	return (0);
 }
