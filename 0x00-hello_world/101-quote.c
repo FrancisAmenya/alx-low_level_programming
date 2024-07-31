4291a4b268fe44cbed71dadcb7952e51f7c57aac
FrancisAmenya <amenyafrancis@gmail.com>
2024-07-31 15:44:42 -0700

    0x00 fix 1b

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 #include <unistd.h>
 
 /**
- * main - prints an assigned text then goes to a new li to the standardd error
- * Return: 0 is a good output, others have failed
+ * main - Entry point
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: as intructed 1 (Success)
  */
+
 int main(void)
 {
-	char *s;
-
-	s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, s + '0', 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
