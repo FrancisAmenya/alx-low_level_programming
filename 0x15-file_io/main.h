edc17322a9ad0a2f69215b3ce9975b09eb797552
FrancisAmenya <amenyafrancis@gmail.com>
2024-08-01 15:20:23 -0700

    0x15 File IO fix 100e

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,7 +10,6 @@
 #include <fcntl.h>
 #include <elf.h>
 
-void close_file(int fd);
 int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
